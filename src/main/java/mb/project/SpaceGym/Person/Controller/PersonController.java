ef77e734d85964d8b1ce970e1ff05163dceebeb5
mateusDbarros <mb685212@gmail.com>
2025-03-03 02:21:33 -0300

    Atualizando

--- src/main/java/mb/project/SpaceGym/Person/Controller/PersonController.java
+++ src/main/java/mb/project/SpaceGym/Person/Controller/PersonController.java
@@ -1,8 +1,14 @@
 package mb.project.SpaceGym.Person.Controller;
 
+import mb.project.SpaceGym.Person.Models.Person;
 import mb.project.SpaceGym.Person.Service.PersonService;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/Person")
@@ -15,15 +21,32 @@ public class PersonController {
     }
 
     @PostMapping
-    public ResponseEntity<String>
-
+    public ResponseEntity<String> addPerson(@RequestBody Person person) {
+        services.addPerson(person);
+        return ResponseEntity.status(HttpStatus.CREATED).body(person.getName() + " adicionada com sucesso!");
+    }
 
     @GetMapping
-
+    public ResponseEntity<List<Person>> listAll() {
+        List<Person> personList = services.personList();
+        return ResponseEntity.status(HttpStatus.OK).body(personList);
+    }
     @GetMapping("/id")
-
+    public ResponseEntity<Optional<Person>> findById(@PathVariable long id) {
+        Optional<Person> person = services.findById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(person);
+    }
 
     @PostMapping("/id")
+    public ResponseEntity<String> updatePerson(@RequestBody Person person, @PathVariable long id) {
+        person.setPersonId(id);
+        services.updatePerson(person);
+        return ResponseEntity.status(HttpStatus.OK).body("Dados de "+person.getName()+ " atualizada(o) com sucesso!");
+    }
 
     @DeleteMapping("/id")
+    public ResponseEntity<String> deletePerson(@PathVariable long id) {
+        services.deletePerson(id);
+        return ResponseEntity.status(HttpStatus.OK).body("Dados excluidos com sucesso!");
+    }
 }
